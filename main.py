d1857be414f58e4a4de579af9f4080116334e19b
Lu <gelululuyuan@gmail.com>
2025-10-02 21:02:20 +0800

    Implement a minimal browser use case

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from nash-tavern!")
+from browser_use import Agent, ChatGoogle, ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
 
+load_dotenv()
+
+async def main():
+    llm = ChatGoogle(model ="gemini-flash-latest")
+    task = "Find the software engineer related jobs at oklo official website"
+    agent = Agent(task=task, llm=llm)
+    await agent.run()
 
 if __name__ == "__main__":
-    main()
+    history = asyncio.run(main())
+    print(history.structured_output)
